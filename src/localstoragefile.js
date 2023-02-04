4c3cec93f640c958c1bda97cdbd5e3457bbd0f25
kingnoel <kingno3l@icloud.com>
2023-02-04 17:14:49 +0100

    linters for js fixed

--- src/localstoragefile.js
+++
@@ -1,9 +0,0 @@
-export default class LocalStorageFile {
-    static getData() {
-      return JSON.parse(localStorageFile.getItem('taskslist'));
-    }
-  
-    static saveData(tasksList) {
-      localStorageFile.setItem('taskslist', JSON.stringify(tasksList));
-    }
-  }
